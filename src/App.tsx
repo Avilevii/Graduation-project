1fbb5d2dbde561da96b30926331dcdcb89b02552
Avraham Levi <lwybrhm048@gmail.com>
2025-09-07 12:08:33 +0300

    step 1 componente post

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import Post from "./comps/Post";
+
+export default function App() {
+  return (
+    <div>
+      <Post/>
+    </div>
+  )
+}
---
+++ src/comps/Post.tsx
@@ -0,0 +1,12 @@
+import'../compsCss/Post.css'
+
+export default function post() {
+  return (
+    <div className="container">
+       <img src="https://lh3.googleusercontent.com/drive-storage/AJQWtBP7HI0F6cfj3WornqbQIm3FHXIe2UCn8quZHK5SrZIr1uDFhJHEGlrzOanOEObVQAdmf36CXk3g-rn6eHFOE5ywn6kluphSc-5-B0qUCd9RW0E=w1920-h912" alt="post img" width={"40%"} height={"40%"}/>
+       <p>Description: logo project</p>
+       <p>Likes: 10</p>
+       <p>Name of the person posting the post: "Avraham Levi"</p>
+<p>The event is scheduled for <time dateTime="2025-09-07">September 07, 2025, at 11:30 AM</time>.</p>    </div>
+  )
+}
